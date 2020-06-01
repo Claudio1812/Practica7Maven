cdc8fc1459f927276eca9550e45d4ecfdba3066f
Claudio Rodriguez Lopez <claudio.rodriguez.smr@gmail.com>
2020-06-01 22:56:30 +0200

    Version estable en en eclipse

--- src/test/ual/hmis/sesion06/Ejercicio5Test.java
+++ src/test/ual/hmis/sesion06/Ejercicio5Test.java
@@ -12,8 +12,6 @@ import ual.hmis.sesion06.Profesor;
 
 
 public class Ejercicio5Test {
-
-	
 	@Test
 	public void test() {
 		Actividad actividad = new Actividad();
@@ -26,22 +24,17 @@ public class Ejercicio5Test {
 
 		Alumno federico = new Alumno();
 
-		
-		
 		ArrayList<Ejercicio> contenedorEjer = new ArrayList<Ejercicio>();
 		ArrayList<Actividad> contenedorAc = new ArrayList<Actividad>();
 		ArrayList<Alumno> contenedorAlum = new ArrayList<Alumno>();
 		
 		contenedorAlum.add(federico);
-
 		Profesor federica=new Profesor(contenedorAlum);
 
 		primero.setNombre("primero");
 		primero.setPuntuacion(4.7);
-		
 		segundo.setNombre("segundo");
 		segundo.setPuntuacion(25.0);
-		
 		tercero.setNombre("tercero");
 		tercero.setPuntuacion(11.2);
 		
@@ -50,9 +43,7 @@ public class Ejercicio5Test {
 		
 		actividad.setEjercicios(contenedorEjer);
 		actividad.setNombre("primero");
-		
 		actividad1.agregarEjercicioCalificado("ejer", 10.0);
-		
 		actividad1.setApta(false);
 		
 		contenedorAc.add(actividad);
@@ -61,16 +52,13 @@ public class Ejercicio5Test {
 		
 		federico.setActividadesAsignadas(contenedorAc);
 		federico.setNombre("federico");
-
 		federica.calificarAlumnos();
 		
 		assertEquals(primero.getNombre(),"primero");
-
 		assertEquals("federico",federico.getNombre());
 		assertEquals(contenedorAc,federico.getActividadesAsignadas());
 		assertEquals(federico.calcularNotaActividad("primero"),29.7,0.01);
 		assertEquals(federico.calcularNotaActividad("primero"),actividad.getPuntuacionTotal(),0.01);
-		
 		assertEquals(actividad1.isApta(),true);
 		
 		
--- src/test/ual/hmis/sesion06/Ejercicio8Test.java
+++ src/test/ual/hmis/sesion06/Ejercicio8Test.java
@@ -14,7 +14,6 @@ public class Ejercicio8Test {
 	@Test
 	public void test() {
 		ArrayList<Vehiculo> contenedor = new ArrayList<Vehiculo>();
-		
 		Ferry barco = new Ferry();
 		Ferry barco1 = new Ferry();
 		
@@ -27,7 +26,6 @@ public class Ejercicio8Test {
 		coche2.setPesoConCarga(450);
 		coche3.setPesoConCarga(320);
 		coche4.setPesoConCarga(2560);
-		
 		coche1.setNumeroPasajeros(6);
 		coche1.setNumeroRuedas(4);
 		coche1.setPesoConCarga(1200);
@@ -38,26 +36,21 @@ public class Ejercicio8Test {
 		
 		barco1.maxPeso=0;
 		barco1.maxVehiculos=0;
-
 		barco.maxOcupantes=300;
 		barco.maxVehiculos=2;
 		barco.setVehiculos(contenedor);
 		barco.getVehiculos();
 
-
 		assertEquals(barco.superadoMaximoPeso(),false);
 		assertEquals(barco.superadoMaximoVehiculos(),false);
 		
 		barco.embarcarVehiculo(coche4);
-		
 		assertEquals(barco.superadoMaximoPeso(),true);
 		assertEquals(barco.vacio(),false);
 		assertEquals(barco.totalVehiculos(),4);
-		
 		assertEquals(coche1.getNumeroPasajeros(),6);
 		assertEquals(coche1.getNumeroRuedas(),4);
 		assertEquals(coche1.getPesoConCarga(),1200);
-		
 		assertEquals(barco1.vacio(),true);
 		assertEquals(barco1.superadoMaximoVehiculos(),false);
 		assertEquals(barco1.superadoMaximoPeso(),false);
